f790ba8230094e8dec44ae2199241e498480b63b
onurtaskirancom <taskiranonur9@gmail.com>
2022-11-24 13:51:48 +0300

    React context and hooks

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,4 @@
+import ToggleTheme from "../components/ToggleTheme";
 import {Button} from "antd";
 
 function Home() {
@@ -5,6 +6,8 @@ function Home() {
     <div>
       <h1>Home</h1>
       <Button type="primary">Click me</Button>
+      <br/>
+      <ToggleTheme/>
     </div>
   );
 }
